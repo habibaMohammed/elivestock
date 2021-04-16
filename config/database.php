163541525543f981ad137c720fe13897b2a19888
Habiba Mohammed <m.habiba@vivatelecoms.com>
2021-04-16 09:33:38 +0300

    Update Database configuration

--- config/database.php
+++ config/database.php
@@ -45,8 +45,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('db4free.net'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'e_livestock'),
             'username' => env('DB_USERNAME', 'e_livestock_admi'),
